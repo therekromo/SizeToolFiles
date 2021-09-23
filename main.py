05ac46d7651be130f6c7b9b77bdf7ad44cd1ea48
My name <me@example.com>
2021-09-23 13:26:06 -0700

    Create Size Tool Repo with source code for sizetool.py

---
+++ main.py
@@ -0,0 +1,4 @@
+#1 take argparse argument for directory/path (maybe use sysargv instead, no need to unit test this)
+#2 create function to get the size of that directory and subfolders/files in it (make sure this only needs to take in the argparse input)
+#3 create a function for the conversion portion (do this with the code on the pc and make sure it only takes in one argument
+#4 create a function that uses all outputs of the previous 2 functions and puts them into 1 print statement instead 
